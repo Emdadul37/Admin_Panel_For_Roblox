a205efd3fa9b5e9a68054797b5aa1e34d831cfd5
Emdadul37 <gamer.emdadul01@gmail.com>
2026-05-17 21:11:49 +0600

    Update script to use obfuscated version of Phantom Key SYS

--- Nova Script.lua
+++ Nova Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Emdadul37/Admin_Panel_For_Roblox/refs/heads/main/Phantom%20Key%20SYS"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Emdadul37/Admin_Panel_For_Roblox/refs/heads/main/Phantom%20Key%20SYS%20(Obfuscated).lua"))()
