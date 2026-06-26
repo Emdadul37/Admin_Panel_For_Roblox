2de52e27c7017d465f550d3f042542475028debe
Emdadul37 <gamer.emdadul01@gmail.com>
2026-06-26 19:34:03 +0600

    Update

--- Nova Script.lua
+++ Nova Script.lua
@@ -1,1 +1,7 @@
-hiiii
+local StarterGui = game:GetService("StarterGui")
+    pcall(function()
+        StarterGui:SetCore("SendNotification", { Title = "Execution Blocked", Text = "The Script Is Under Maintenance. We Will Come Back Soon", Duration = 10 })
+    end)
+    game.Players.LocalPlayer:Kick("Phantom: Bypass Detected. Please use the official Key System.")
+    return
+end
